9f8f212e9b4f4d6253ff7615925c7d38db8a5319
Ynnon <140154750+thefeshch@users.noreply.github.com>
2024-10-15 00:14:18 +0300

    Update lab4.cpp

--- lab4.cpp
+++ lab4.cpp
@@ -7,8 +7,7 @@ if (number == 5)
 	std::cout << "The number is 5" << std::endl;
 	return;
 	}
-else
-	std::cout << "The number is not 5" << std::endl;
+std::cout << "The number is not 5" << std::endl;
 	
 	
 if (number % 2 == 1)
@@ -16,8 +15,7 @@ if (number % 2 == 1)
 	std::cout << "The number is not divisible by 2" << std::endl;
 	return;
 	}
-else
-	std::cout << "The number is divisible by 2" << std::endl;
+std::cout << "The number is divisible by 2" << std::endl;
 	
 	
 if (number == 6)
