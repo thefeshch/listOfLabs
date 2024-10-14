e04896b56164cb423b147dafba8a91c2415e6ef2
Ynnon <140154750+thefeshch@users.noreply.github.com>
2024-10-14 19:26:22 +0300

    Update lab4.cpp

--- lab4.cpp
+++ lab4.cpp
@@ -2,33 +2,32 @@
 
 void f(int number)
 {
-	if (number == 5)
-		{
-			std::cout << "The number is 5" << std::endl;
-			return;
-		}
-    else
-		std::cout << "The number is not 5" << std::endl;
+if (number == 5)
+	{
+	std::cout << "The number is 5" << std::endl;
+	return;
+	}
+else
+	std::cout << "The number is not 5" << std::endl;
 	
 	
-	if (number % 2 == 1)
-        {
-            std::cout << "The number is not divisible by 2" << std::endl;
-			return;
-        }
-	else
-		std::cout << "The number is divisible by 2" << std::endl;
+if (number % 2 == 1)
+	{
+	std::cout << "The number is not divisible by 2" << std::endl;
+	return;
+	}
+else
+	std::cout << "The number is divisible by 2" << std::endl;
 	
 	
-	if (number == 6)
-            {
-                std::cout << "The number is 6" << std::endl;
-				return;
-            }
+if (number == 6)
+	{
+	std::cout << "The number is 6" << std::endl;
+	return;
+	}
 }
 int main()
 {
     int number = 10;
-	f(number);
-
+    f(number);
 }
